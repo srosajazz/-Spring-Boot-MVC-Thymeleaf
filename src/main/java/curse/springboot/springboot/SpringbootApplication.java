d73a8272724e7d7d88b0461dafbb12decba7c427
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-06 15:27:18 -0400

    Configuring JPA + Database

--- src/main/java/curse/springboot/springboot/SpringbootApplication.java
+++ src/main/java/curse/springboot/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package curse.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages="curse.springboot.springboot.model")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
