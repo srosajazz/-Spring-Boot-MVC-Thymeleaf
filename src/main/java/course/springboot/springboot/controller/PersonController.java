0c9045d79e99cbc6d42267f5f5dfb1d7707d6579
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-08 09:22:10 -0400

    Search by name

--- src/main/java/course/springboot/springboot/controller/PersonController.java
+++ src/main/java/course/springboot/springboot/controller/PersonController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import course.springboot.springboot.model.Person;
@@ -26,6 +28,7 @@ public class PersonController {
 		return modelAndView;
 	}
 
+	// SAVEPERSON
 	@RequestMapping(method = RequestMethod.POST, value = "**/saveperson")
 	public ModelAndView save(Person person) {
 		personRepository.save(person);
@@ -37,6 +40,7 @@ public class PersonController {
 		return andView;
 	}
 
+	// PERSON LIST
 	@RequestMapping(method = RequestMethod.GET, value = "/personlist")
 	public ModelAndView persons() {
 		ModelAndView andView = new ModelAndView("registration/personregistration");
@@ -45,15 +49,34 @@ public class PersonController {
 		andView.addObject("personobj", new Person());
 		return andView;
 	}
-	// EDIT
 
+	// EDIT
 	@GetMapping("/editperson/{idperson}")
 	public ModelAndView edit(@PathVariable("idperson") Long idperson) {
-		
+
 		Optional<Person> person = personRepository.findById(idperson);
 		ModelAndView modelAndView = new ModelAndView("registration/personregistration");
 		modelAndView.addObject("personobj", person.get());
 		return modelAndView;
+	}
+
+	// DELETE
+	@GetMapping("/removeperson/{idperson}")
+	public ModelAndView delete(@PathVariable("idperson") Long idperson) {
+		personRepository.deleteById(idperson);
+		ModelAndView modelAndView = new ModelAndView("registration/personregistration");
+		modelAndView.addObject("personobj", personRepository.findAll());
+		modelAndView.addObject("personobj", new Person());
+		return modelAndView;
+	}
+	// SEARCH
+	@PostMapping("**/searchperson")
+	public ModelAndView search(@RequestParam("namesearch") String namesearch) {
+		ModelAndView modelAndView = new ModelAndView("registration/personregistration");
+		modelAndView.addObject("persons", personRepository.findPersonByName(namesearch));
+		modelAndView.addObject("personobj", new Person());
+		return modelAndView;
+
 	}
 
 }
--- src/main/java/course/springboot/springboot/repository/PersonRepository.java
+++ src/main/java/course/springboot/springboot/repository/PersonRepository.java
@@ -1,5 +1,8 @@
 package course.springboot.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,7 @@ import course.springboot.springboot.model.Person;
 @Transactional
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+	@Query("select p from Person p where p.name like %?1% ")
+	List<Person> findPersonByName(String name);
+	
 }
